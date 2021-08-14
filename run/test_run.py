09ae92004d7ba28e3e575b7247ce7a61b0a9aecb
Xiong <jie.xiong@nrel.gov>
2021-08-14 04:00:34 -0600

    Add cross validation feature

--- run/test_run.py
+++ run/test_run.py
@@ -1,6 +1,7 @@
 from FDD_RF_Module.FDD_RF_Modeling import FDD_RF_Modeling
 
 test_run = FDD_RF_Modeling(weather = 'Norfolk', labeling_methodolog = 'Simple',
-                           feature_selection_methodology = 'Embedded', aggregate_n_runs = 4 * 24, number_of_trees = 2)
+                           feature_selection_methodology = 'Embedded', aggregate_n_runs = 6 * 24, number_of_trees = 4,
+                           randomseed=20210813)
 
 test_run.whole_process_training_and_testing()

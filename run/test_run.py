29c349cd11371f65d5e03839289ca7fb5f84dd44
Janghyun(JK)Kim <janghyun.kim@nrel.gov>
2022-01-07 11:58:26 -0700

    updating test_run script

--- run/test_run.py
+++ run/test_run.py
@@ -22,10 +22,11 @@ i = 0
 for x in randomseedpool:
     i += 1
     print('Starting run ' + str(i))
-    test_run = FDD_RF_Modeling(weather = configs['weather'], 
+    test_run = FDD_RF_Modeling(configs,
+                               weather = configs['weather'], 
                                labeling_methodology = configs['labeling_methodology'],
                                feature_selection_methodology = configs['feature_selection_methodology'], 
-                               aggregate_n_runs = int(60/configs['simulation_timestep_min']*configs["fdd_reporting_frequency_hrs"])
+                               fdd_reporting_frequency_hrs = configs["fdd_reporting_frequency_hrs"],
                                number_of_trees = configs['number_of_trees'],
                                randomseed=x)
     test_run.whole_process_training_and_testing()

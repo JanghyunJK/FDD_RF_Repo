dade1c9c119241a4ffdd053ad4a2674e13e1bb27
Janghyun(JK)Kim <janghyun.kim@nrel.gov>
2022-01-07 11:06:12 -0700

    Update test_run.py

--- run/test_run.py
+++ run/test_run.py
@@ -1,16 +1,31 @@
 from FDD_RF_Module.FDD_RF_Modeling import FDD_RF_Modeling
 import random
+import json
 
-randomruns = 10
-randomseedpool = random.sample(range(10000), k=randomruns)
+# setting directories
+dir_code = "."
+dir_data = "./data"
+
+# setting configs json file path
+file_configs = dir_code + "/configs.json"
+
+# reading configs json file
+print("reading configuration json file from = {}".format(file_configs))
+with open(file_configs, "r") as read_file:
+    configs = json.load(read_file)
+
+# adding dir_data into configs json file
+configs['dir_data'] = dir_data
+
+randomseedpool = random.sample(range(10000), k=configs['randomruns'])
 i = 0
 for x in randomseedpool:
     i += 1
     print('Starting run ' + str(i))
-    test_run = FDD_RF_Modeling(weather = 'Norfolk', 
-                               labeling_methodology = 'Simple',
-                               feature_selection_methodology = 'Embedded', 
-                               aggregate_n_runs = 6 * 24, 
-                               number_of_trees = 200,
+    test_run = FDD_RF_Modeling(weather = configs['weather'], 
+                               labeling_methodology = configs['labeling_methodology'],
+                               feature_selection_methodology = configs['feature_selection_methodology'], 
+                               aggregate_n_runs = int(60/configs['simulation_timestep_min']*configs["fdd_reporting_frequency_hrs"])
+                               number_of_trees = configs['number_of_trees'],
                                randomseed=x)
     test_run.whole_process_training_and_testing()
